7d72c333b880271a817929d60935e2a00544f87a
brunobbr <brunobrdev@gmail.com>
2021-02-24 20:51:10 -0400

    countdown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 
 import styles from '../styles/pages/Home.module.css';
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/countdown";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           <div>
             <Profile />
             <CompletedChallenges />
+            <Countdown />
           </div>
           <div></div>
 
